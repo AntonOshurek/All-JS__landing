51d2f8144e70d3e6bdf1f3748457a60dfcceaeac
AntonOshurek <oshurekweb@gmail.com>
2021-09-24 12:40:57 +0200

    add basic script logick for popup

--- source/scripts/blocks/popup.js
+++ source/scripts/blocks/popup.js
@@ -1,3 +1,30 @@
 export default function popup() {
+  const body = document.querySelector('.body');
+  const popupOpenBtn = document.querySelector('.popup-block__btn');
+  const popup = document.querySelector('.popup');
+  const popupArea = document.querySelector('.popup__area');
+  const popupCloseBtn = document.querySelector('.popup__close-btn');
 
+  const closeModal = (e) => {
+    e.preventDefault();
+    popup.classList.remove('popup--target');
+    body.classList.remove("body--scrolloff");
+  };
+
+  const openModal = (e) => {
+    e.preventDefault();
+    popup.classList.add('popup--target');
+    body.classList.add("body--scrolloff");
+    modalKeyOpt();
+  };
+
+  function modalKeyOpt () {
+    window.onkeydown = ( event ) => {
+      if ( event.keyCode == 27 ) closeModal(event);
+    };
+  };
+
+  popupOpenBtn.addEventListener ('click', (e) => openModal(e));
+  popupCloseBtn.addEventListener('click', (e) => closeModal(e));
+  popupArea.addEventListener('click', (e) => closeModal(e));
 };
