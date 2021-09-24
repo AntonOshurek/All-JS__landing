967d2d895a867415db3c003de2bb2518b6b2373e
AntonOshurek <oshurekweb@gmail.com>
2021-09-24 11:03:37 +0200

    add basic js scripts for tabs

--- source/scripts/blocks/tabs.js
+++ source/scripts/blocks/tabs.js
@@ -1,3 +1,24 @@
 export default function tabs() {
+  const tab = function() {
+    const tab = document.querySelectorAll('.tab');
+    const tabButton = document.querySelectorAll('.tabs-controls__btn');
+    let tabName;
 
+    tabButton.forEach(btn => {
+        btn.addEventListener('click', selectTabNav)
+    });
+
+    function selectTabNav() {
+        tabName = this.getAttribute('data-tab-name');
+        selectTabContent(tabName);
+    }
+
+    function selectTabContent(tabName) {
+        tab.forEach(item => {
+            item.classList.contains(tabName) ? item.classList.add('tab--current') : item.classList.remove('tab--current');
+        })
+    }
+};
+
+tab();
 };
