cb586acd0220b83e792b7d95046958235a9d331d
AntonOshurek <oshurekweb@gmail.com>
2021-09-24 13:38:07 +0200

    add pagination controls buttons

--- source/scripts/index.js
+++ source/scripts/index.js
@@ -2,11 +2,13 @@ import slider from './blocks/slider';
 import sliderFull from './blocks/sliderfull';
 import tabs from './blocks/tabs';
 import popup from './blocks/popup';
+import pagination from './blocks/pagination';
 
 window.addEventListener('DOMContentLoaded', () => {
     slider();
     sliderFull();
     tabs();
     popup();
+    pagination();
 });
 
