67650457003cab45783617d121b2d839c3f81a31
AntonOshurek <oshurekweb@gmail.com>
2021-10-03 23:41:49 +0200

    adding script logick for burger btn

--- source/scripts/index.js
+++ source/scripts/index.js
@@ -3,8 +3,10 @@ import sliderFull from './blocks/sliderfull';
 import tabs from './blocks/tabs';
 import popup from './blocks/popup';
 import pagination from './blocks/pagination';
+import navigationBurgerBtn from './blocks/navigationBurgerBtn';
 
 window.addEventListener('DOMContentLoaded', () => {
+  navigationBurgerBtn();
     slider();
     sliderFull();
     tabs();
