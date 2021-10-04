da0d7484414a6e3a8b60d050b5fff785102cf034
AntonOshurek <oshurekweb@gmail.com>
2021-10-04 22:31:08 +0200

    adding new resize screen function for close burger menu

--- source/scripts/blocks/navigationBurgerBtn.js
+++ source/scripts/blocks/navigationBurgerBtn.js
@@ -4,6 +4,7 @@ export  default function navigationBurgerBtn() {
   const navBtnBurger = document.querySelector('.nav__btn-burger');
   const navList = document.querySelector('.nav__list');
   const navLink = document.querySelectorAll('.nav__link');
+  const noTouchBG = document.querySelector('.nav__background-notouch');
 
   let navOpen = false;
 
@@ -20,6 +21,7 @@ export  default function navigationBurgerBtn() {
     navBtnBurger.classList.add('nav__btn-burger--open');
     navList.classList.add('nav__list--open');
     body.classList.add('body--scrolloff');
+    noTouchBG.classList.add('nav__background-notouch--on');
 
     navLink.forEach((link) => {
       link.addEventListener('click', () => {
@@ -35,6 +37,15 @@ export  default function navigationBurgerBtn() {
     navBtnBurger.classList.remove('nav__btn-burger--open');
     navList.classList.remove('nav__list--open');
     body.classList.remove('body--scrolloff');
+    noTouchBG.classList.remove('nav__background-notouch--on');
     navOpen = false;
   };
+
+  window.addEventListener('resize', (e) => {
+    const screenWidth = window.screen.width;
+    if (screenWidth > 900) {
+      closeNavigation();
+    };
+  });
+
 }
