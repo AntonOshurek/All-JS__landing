61634c86ac3dd89fb6df0ee5bf5b9ec89c320e72
AntonOshurek <oshurekweb@gmail.com>
2021-10-04 17:17:08 +0200

    adding js logick for open and close navigation

--- source/scripts/blocks/navigationBurgerBtn.js
+++ source/scripts/blocks/navigationBurgerBtn.js
@@ -1,16 +1,20 @@
 export  default function navigationBurgerBtn() {
   const navBtn = document.querySelector('.nav__btn');
   const navBtnBurger = document.querySelector('.nav__btn-burger');
+  const navList = document.querySelector('.nav__list');
+
   let navOpen = false;
 
   navBtn.addEventListener('click', () => {
     if(!navOpen) {
       navBtn.classList.add('nav__btn--open');
       navBtnBurger.classList.add('nav__btn-burger--open');
+      navList.classList.add('nav__list--open');
       navOpen = true;
     } else {
       navBtn.classList.remove('nav__btn--open');
       navBtnBurger.classList.remove('nav__btn-burger--open');
+      navList.classList.remove('nav__list--open');
       navOpen = false;
     }
   })
