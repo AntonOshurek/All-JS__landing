f2cb9b969cbb23c9dd061a6b2e9adfb4af7bb95e
AntonOshurek <oshurekweb@gmail.com>
2021-10-04 17:54:52 +0200

    close nav when event on link logick

--- source/scripts/blocks/navigationBurgerBtn.js
+++ source/scripts/blocks/navigationBurgerBtn.js
@@ -3,22 +3,39 @@ export  default function navigationBurgerBtn() {
   const navBtn = document.querySelector('.nav__btn');
   const navBtnBurger = document.querySelector('.nav__btn-burger');
   const navList = document.querySelector('.nav__list');
+  const navLink = document.querySelectorAll('.nav__link');
 
   let navOpen = false;
 
   navBtn.addEventListener('click', () => {
     if(!navOpen) {
-      navBtn.classList.add('nav__btn--open');
-      navBtnBurger.classList.add('nav__btn-burger--open');
-      navList.classList.add('nav__list--open');
-      body.classList.add('body--scrolloff');
-      navOpen = true;
+      openNavigation();
     } else {
-      navBtn.classList.remove('nav__btn--open');
-      navBtnBurger.classList.remove('nav__btn-burger--open');
-      navList.classList.remove('nav__list--open');
-      body.classList.remove('body--scrolloff');
-      navOpen = false;
+      closeNavigation();
     }
+  });
+
+  navLink.forEach((link) => {
+    link.addEventListener('click', () => {
+      closeNavigation();
+    });
   })
+
+
+
+  function openNavigation() {
+    navBtn.classList.add('nav__btn--open');
+    navBtnBurger.classList.add('nav__btn-burger--open');
+    navList.classList.add('nav__list--open');
+    body.classList.add('body--scrolloff');
+    navOpen = true;
+  }
+
+  function closeNavigation() {
+    navBtn.classList.remove('nav__btn--open');
+    navBtnBurger.classList.remove('nav__btn-burger--open');
+    navList.classList.remove('nav__list--open');
+    body.classList.remove('body--scrolloff');
+    navOpen = false;
+  }
 }
